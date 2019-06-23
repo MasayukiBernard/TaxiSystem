b5c724568ca86d990448405c689d7aba617dbc8c
Amadeus Darren Leander <41075353+adarrenleander@users.noreply.github.com>
2019-06-23 16:29:18 +0700

    Completed first report form
    Number of uses per service. Can filter per month and per year.

--- Application/BlueBirdSystem/BlueBirdSystem/Report_MonthlyUsesPerServiceForm.Designer.cs
+++ Application/BlueBirdSystem/BlueBirdSystem/Report_MonthlyUsesPerServiceForm.Designer.cs
@@ -28,12 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbYear = new System.Windows.Forms.ComboBox();
+            this.dgvOrders = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbMonth = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbService = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUses = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(135, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Year";
+            // 
+            // cbYear
+            // 
+            this.cbYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbYear.FormattingEnabled = true;
+            this.cbYear.Items.AddRange(new object[] {
+            "-- Choose Year --",
+            "2019"});
+            this.cbYear.Location = new System.Drawing.Point(220, 54);
+            this.cbYear.Name = "cbYear";
+            this.cbYear.Size = new System.Drawing.Size(219, 33);
+            this.cbYear.TabIndex = 2;
+            this.cbYear.SelectedIndexChanged += new System.EventHandler(this.CbYear_SelectedIndexChanged);
+            // 
+            // dgvOrders
+            // 
+            this.dgvOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrders.Location = new System.Drawing.Point(30, 179);
+            this.dgvOrders.Name = "dgvOrders";
+            this.dgvOrders.RowHeadersWidth = 51;
+            this.dgvOrders.RowTemplate.Height = 24;
+            this.dgvOrders.Size = new System.Drawing.Size(1037, 319);
+            this.dgvOrders.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(135, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Month";
+            // 
+            // cbMonth
+            // 
+            this.cbMonth.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbMonth.FormattingEnabled = true;
+            this.cbMonth.Items.AddRange(new object[] {
+            "-- Choose Month --",
+            "January",
+            "Febuary",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"});
+            this.cbMonth.Location = new System.Drawing.Point(220, 108);
+            this.cbMonth.Name = "cbMonth";
+            this.cbMonth.Size = new System.Drawing.Size(219, 33);
+            this.cbMonth.TabIndex = 2;
+            this.cbMonth.SelectedIndexChanged += new System.EventHandler(this.CbMonth_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(540, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Service Type";
+            // 
+            // cbService
+            // 
+            this.cbService.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbService.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbService.FormattingEnabled = true;
+            this.cbService.Items.AddRange(new object[] {
+            "-- Choose Service Type --",
+            "Blue Bird",
+            "Silver Bird",
+            "Big Bird"});
+            this.cbService.Location = new System.Drawing.Point(692, 54);
+            this.cbService.Name = "cbService";
+            this.cbService.Size = new System.Drawing.Size(219, 33);
+            this.cbService.TabIndex = 2;
+            this.cbService.SelectedIndexChanged += new System.EventHandler(this.CbService_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(298, 539);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 32);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Total Uses:";
+            // 
+            // txtUses
+            // 
+            this.txtUses.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUses.Location = new System.Drawing.Point(462, 536);
+            this.txtUses.Name = "txtUses";
+            this.txtUses.Size = new System.Drawing.Size(225, 38);
+            this.txtUses.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(693, 539);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 32);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "trips";
+            // 
+            // Report_MonthlyUsesPerServiceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Report_MonthlyUsesPerServiceForm";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1104, 625);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtUses);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dgvOrders);
+            this.Controls.Add(this.cbService);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbMonth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbYear);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Report_MonthlyUsesPerServiceForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Blue Bird - Report: Monthly Uses per Service";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbYear;
+        private System.Windows.Forms.DataGridView dgvOrders;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbMonth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbService;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUses;
+        private System.Windows.Forms.Label label5;
     }
 }
--- Application/BlueBirdSystem/BlueBirdSystem/Report_MonthlyUsesPerServiceForm.cs
+++ Application/BlueBirdSystem/BlueBirdSystem/Report_MonthlyUsesPerServiceForm.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Db4objects.Db4o.Linq;
+using BlueBirdSystem.data;
+using BlueBirdSystem.model;
 
 namespace BlueBirdSystem
 {
@@ -15,6 +18,328 @@ namespace BlueBirdSystem
         public Report_MonthlyUsesPerServiceForm()
         {
             InitializeComponent();
+            resetInitial();
+            showData();
+        }
+
+        public void resetInitial()
+        {
+            txtUses.Enabled = false;
+            cbYear.SelectedIndex = 0;
+            cbMonth.SelectedIndex = 0;
+            cbService.SelectedIndex = 0;
+            cbYear.Enabled = false;
+            cbMonth.Enabled = false;
+        }
+
+        public void showData()
+        {
+            if (cbService.SelectedIndex == 0)
+            {
+                var getOrders = (from Order o in DB.conn
+                                 join BlueBird b in DB.conn
+                                 on o.vehicleID equals b.ID
+                                 select new
+                                 {
+                                     OrderID = o.ID,
+                                     OrderDate = o.orderDate,
+                                     CustomerID = o.customerID,
+                                     VehicleID = o.ID,
+                                     ServiceType = "Blue Bird",
+                                     VehicleType = b.type,
+                                     PickupLocation = o.pickupLocation,
+                                     DestinationLocation = o.destinationLocation,
+                                     PickupDate = o.pickupDate
+                                 }).Union(
+                             from Order o in DB.conn
+                             join SilverBird b in DB.conn
+                             on o.vehicleID equals b.ID
+                             select new
+                             {
+                                 OrderID = o.ID,
+                                 OrderDate = o.orderDate,
+                                 CustomerID = o.customerID,
+                                 VehicleID = o.ID,
+                                 ServiceType = "Silver Bird",
+                                 VehicleType = b.type,
+                                 PickupLocation = o.pickupLocation,
+                                 DestinationLocation = o.destinationLocation,
+                                 PickupDate = o.pickupDate
+                             }).Union(
+                             from Order o in DB.conn
+                             join BigBird b in DB.conn
+                             on o.vehicleID equals b.ID
+                             select new
+                             {
+                                 OrderID = o.ID,
+                                 OrderDate = o.orderDate,
+                                 CustomerID = o.customerID,
+                                 VehicleID = o.ID,
+                                 ServiceType = "Big Bird",
+                                 VehicleType = b.type,
+                                 PickupLocation = o.pickupLocation,
+                                 DestinationLocation = o.destinationLocation,
+                                 PickupDate = o.pickupDate
+                             }).ToList();
+
+                dgvOrders.DataSource = getOrders;
+                txtUses.Text = getOrders.Count.ToString();
+            }
+            else if (cbService.SelectedIndex == 1)
+            {
+                var getOrders = (from Order o in DB.conn
+                                   join BlueBird b in DB.conn
+                                   on o.vehicleID equals b.ID
+                                   select new
+                                   {
+                                       OrderID = o.ID,
+                                       OrderDate = o.orderDate,
+                                       CustomerID = o.customerID,
+                                       VehicleID = o.ID,
+                                       ServiceType = "Blue Bird",
+                                       VehicleType = b.type,
+                                       PickupLocation = o.pickupLocation,
+                                       DestinationLocation = o.destinationLocation,
+                                       PickupDate = o.pickupDate
+                                   }).ToList();
+
+                dgvOrders.DataSource = getOrders;
+                txtUses.Text = getOrders.Count.ToString();
+            }
+            else if (cbService.SelectedIndex == 2)
+            {
+                var getOrders = (from Order o in DB.conn
+                                 join SilverBird b in DB.conn
+                                 on o.vehicleID equals b.ID
+                                 select new
+                                 {
+                                     OrderID = o.ID,
+                                     OrderDate = o.orderDate,
+                                     CustomerID = o.customerID,
+                                     VehicleID = o.ID,
+                                     ServiceType = "Silver Bird",
+                                     VehicleType = b.type,
+                                     PickupLocation = o.pickupLocation,
+                                     DestinationLocation = o.destinationLocation,
+                                     PickupDate = o.pickupDate
+                                 }).ToList();
+
+                dgvOrders.DataSource = getOrders;
+                txtUses.Text = getOrders.Count.ToString();
+            }
+            else if (cbService.SelectedIndex == 3)
+            {
+                var getOrders = (from Order o in DB.conn
+                                 join BigBird b in DB.conn
+                                 on o.vehicleID equals b.ID
+                                 select new
+                                 {
+                                     OrderID = o.ID,
+                                     OrderDate = o.orderDate,
+                                     CustomerID = o.customerID,
+                                     VehicleID = o.ID,
+                                     ServiceType = "Big Bird",
+                                     VehicleType = b.type,
+                                     PickupLocation = o.pickupLocation,
+                                     DestinationLocation = o.destinationLocation,
+                                     PickupDate = o.pickupDate
+                                 }).ToList();
+
+                dgvOrders.DataSource = getOrders;
+                txtUses.Text = getOrders.Count.ToString();
+            }
+        }
+
+        public void showDataYears()
+        {
+            if (cbYear.SelectedIndex != 0)
+            {
+                var year = Int32.Parse(cbYear.Text);
+
+                if (cbService.SelectedIndex == 1)
+                {
+                    var getOrders = (from Order o in DB.conn
+                                       join BlueBird b in DB.conn
+                                       on o.vehicleID equals b.ID
+                                       where o.pickupDate.Year == year
+                                       select new
+                                       {
+                                           OrderID = o.ID,
+                                           OrderDate = o.orderDate,
+                                           CustomerID = o.customerID,
+                                           VehicleID = o.ID,
+                                           ServiceType = "Blue Bird",
+                                           VehicleType = b.type,
+                                           PickupLocation = o.pickupLocation,
+                                           DestinationLocation = o.destinationLocation,
+                                           PickupDate = o.pickupDate
+                                       }).ToList();
+
+                    dgvOrders.DataSource = getOrders;
+                    txtUses.Text = getOrders.Count.ToString();
+                }
+                else if (cbService.SelectedIndex == 2)
+                {
+                    var getOrders = (from Order o in DB.conn
+                                     join SilverBird b in DB.conn
+                                     on o.vehicleID equals b.ID
+                                     where o.pickupDate.Year == year
+                                     select new
+                                     {
+                                         OrderID = o.ID,
+                                         OrderDate = o.orderDate,
+                                         CustomerID = o.customerID,
+                                         VehicleID = o.ID,
+                                         ServiceType = "Silver Bird",
+                                         VehicleType = b.type,
+                                         PickupLocation = o.pickupLocation,
+                                         DestinationLocation = o.destinationLocation,
+                                         PickupDate = o.pickupDate
+                                     }).ToList();
+
+                    dgvOrders.DataSource = getOrders;
+                    txtUses.Text = getOrders.Count.ToString();
+                }
+                else if (cbService.SelectedIndex == 3)
+                {
+                    var getOrders = (from Order o in DB.conn
+                                     join BigBird b in DB.conn
+                                     on o.vehicleID equals b.ID
+                                     where o.pickupDate.Year == year
+                                     select new
+                                     {
+                                         OrderID = o.ID,
+                                         OrderDate = o.orderDate,
+                                         CustomerID = o.customerID,
+                                         VehicleID = o.ID,
+                                         ServiceType = "Big Bird",
+                                         VehicleType = b.type,
+                                         PickupLocation = o.pickupLocation,
+                                         DestinationLocation = o.destinationLocation,
+                                         PickupDate = o.pickupDate
+                                     }).ToList();
+
+                    dgvOrders.DataSource = getOrders;
+                    txtUses.Text = getOrders.Count.ToString();
+                }
+            }
+        }
+
+        private void CbService_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbYear.SelectedIndex = 0;
+            cbMonth.SelectedIndex = 0;
+
+            if (cbService.SelectedIndex == 0)
+            {
+                cbYear.Enabled = false;
+                cbMonth.Enabled = false;
+                showData();
+            }
+            else
+            {
+                cbYear.Enabled = true;
+                showData();
+            }
+        }
+
+        private void CbYear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbMonth.SelectedIndex = 0;
+
+            if (cbYear.SelectedIndex == 0)
+            {
+                cbMonth.Enabled = false;
+                showData();
+            }
+            else
+            {
+                cbMonth.Enabled = true;
+                showDataYears();
+            }
+        }
+
+        private void CbMonth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbMonth.SelectedIndex == 0)
+            {
+                showDataYears();   
+            }
+            else
+            {
+                var year = Int32.Parse(cbYear.Text);
+                var month = cbMonth.SelectedIndex;
+
+                if (cbService.SelectedIndex == 1)
+                {
+                    var getOrders = (from Order o in DB.conn
+                                       join BlueBird b in DB.conn
+                                       on o.vehicleID equals b.ID
+                                       where o.pickupDate.Year == year
+                                       && o.pickupDate.Month == month
+                                       select new
+                                       {
+                                           OrderID = o.ID,
+                                           OrderDate = o.orderDate,
+                                           CustomerID = o.customerID,
+                                           VehicleID = o.ID,
+                                           ServiceType = "Blue Bird",
+                                           VehicleType = b.type,
+                                           PickupLocation = o.pickupLocation,
+                                           DestinationLocation = o.destinationLocation,
+                                           PickupDate = o.pickupDate
+                                       }).ToList();
+
+                    dgvOrders.DataSource = getOrders;
+                    txtUses.Text = getOrders.Count.ToString();
+                }
+                else if (cbService.SelectedIndex == 2)
+                {
+                    var getOrders = (from Order o in DB.conn
+                                     join SilverBird b in DB.conn
+                                     on o.vehicleID equals b.ID
+                                     where o.pickupDate.Year == year
+                                     && o.pickupDate.Month == month
+                                     select new
+                                     {
+                                         OrderID = o.ID,
+                                         OrderDate = o.orderDate,
+                                         CustomerID = o.customerID,
+                                         VehicleID = o.ID,
+                                         ServiceType = "Silver Bird",
+                                         VehicleType = b.type,
+                                         PickupLocation = o.pickupLocation,
+                                         DestinationLocation = o.destinationLocation,
+                                         PickupDate = o.pickupDate
+                                     }).ToList();
+
+                    dgvOrders.DataSource = getOrders;
+                    txtUses.Text = getOrders.Count.ToString();
+                }
+                else if (cbService.SelectedIndex == 3)
+                {
+                    var getOrders = (from Order o in DB.conn
+                                     join BigBird b in DB.conn
+                                     on o.vehicleID equals b.ID
+                                     where o.pickupDate.Year == year
+                                     && o.pickupDate.Month == month
+                                     select new
+                                     {
+                                         OrderID = o.ID,
+                                         OrderDate = o.orderDate,
+                                         CustomerID = o.customerID,
+                                         VehicleID = o.ID,
+                                         ServiceType = "Big Bird",
+                                         VehicleType = b.type,
+                                         PickupLocation = o.pickupLocation,
+                                         DestinationLocation = o.destinationLocation,
+                                         PickupDate = o.pickupDate
+                                     }).ToList();
+
+                    dgvOrders.DataSource = getOrders;
+                    txtUses.Text = getOrders.Count.ToString();
+                }
+            }
         }
     }
 }
