a0e4e3867805076a67ba0b96904a0a6e673c9d82
Amadeus Darren Leander <41075353+adarrenleander@users.noreply.github.com>
2019-06-24 14:32:07 +0700

    Uploaded fifth report form.

--- Application/BlueBirdSystem/BlueBirdSystem/Report_OngoingTripsForm.Designer.cs
+++ Application/BlueBirdSystem/BlueBirdSystem/Report_OngoingTripsForm.Designer.cs
@@ -28,12 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtOngoing = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvOngoing = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOngoing)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtOngoing
+            // 
+            this.txtOngoing.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOngoing.Location = new System.Drawing.Point(208, 29);
+            this.txtOngoing.Name = "txtOngoing";
+            this.txtOngoing.Size = new System.Drawing.Size(100, 34);
+            this.txtOngoing.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ongoing Trips:";
+            // 
+            // dgvOngoing
+            // 
+            this.dgvOngoing.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOngoing.Location = new System.Drawing.Point(12, 92);
+            this.dgvOngoing.Name = "dgvOngoing";
+            this.dgvOngoing.RowHeadersWidth = 51;
+            this.dgvOngoing.RowTemplate.Height = 24;
+            this.dgvOngoing.Size = new System.Drawing.Size(1198, 364);
+            this.dgvOngoing.TabIndex = 3;
+            // 
+            // Report_OngoingTripsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Report_OngoingTripsForm";
+            this.ClientSize = new System.Drawing.Size(1226, 480);
+            this.Controls.Add(this.txtOngoing);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvOngoing);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Report_OngoingTripsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Blue Bird - Report: Ongoing Trips";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOngoing)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtOngoing;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvOngoing;
     }
 }
--- Application/BlueBirdSystem/BlueBirdSystem/Report_OngoingTripsForm.cs
+++ Application/BlueBirdSystem/BlueBirdSystem/Report_OngoingTripsForm.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Db4objects.Db4o.Linq;
+using BlueBirdSystem.data;
+using BlueBirdSystem.model;
 
 namespace BlueBirdSystem
 {
@@ -15,6 +18,30 @@ namespace BlueBirdSystem
         public Report_OngoingTripsForm()
         {
             InitializeComponent();
+            showData();
+        }
+
+        public void showData()
+        {
+            var getTrips = (from Order x in DB.conn
+                            where x.completed == false
+                            select new
+                            {
+                                x.ID,
+                                x.orderDate,
+                                x.customerID,
+                                x.driverID,
+                                x.vehicleID,
+                                x.pickupLocation,
+                                x.pickupNotes,
+                                x.destinationLocation,
+                                x.destinationNotes,
+                                x.pickupDate,
+                                x.passengers
+                            }).ToList();
+
+            dgvOngoing.DataSource = getTrips;
+            txtOngoing.Text = getTrips.Count.ToString();
         }
     }
 }
