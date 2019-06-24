f12750671df219871e309e83bcfc10afce98697c
Amadeus Darren Leander <41075353+adarrenleander@users.noreply.github.com>
2019-06-24 14:49:00 +0700

    Uploaded sixth report form. Updated word document report accordingly.

--- Application/BlueBirdSystem/BlueBirdSystem/Report_DriverHistoryForm.Designer.cs
+++ Application/BlueBirdSystem/BlueBirdSystem/Report_DriverHistoryForm.Designer.cs
@@ -28,12 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvDriver = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDriver)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDriver
+            // 
+            this.dgvDriver.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDriver.Location = new System.Drawing.Point(68, 74);
+            this.dgvDriver.Name = "dgvDriver";
+            this.dgvDriver.RowHeadersWidth = 51;
+            this.dgvDriver.RowTemplate.Height = 24;
+            this.dgvDriver.Size = new System.Drawing.Size(632, 332);
+            this.dgvDriver.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Driver with most trips:";
+            // 
+            // Report_DriverHistoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Report_DriverHistoryForm";
+            this.Controls.Add(this.dgvDriver);
+            this.Controls.Add(this.label1);
+            this.Name = "Report_DriverHistoryForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Blue Bird - Report: Driver Trip History";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDriver)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvDriver;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Application/BlueBirdSystem/BlueBirdSystem/Report_DriverHistoryForm.cs
+++ Application/BlueBirdSystem/BlueBirdSystem/Report_DriverHistoryForm.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Db4objects.Db4o.Linq;
+using BlueBirdSystem.data;
+using BlueBirdSystem.model;
 
 namespace BlueBirdSystem
 {
@@ -15,6 +18,25 @@ namespace BlueBirdSystem
         public Report_DriverHistoryForm()
         {
             InitializeComponent();
+            showData();
+        }
+
+        public void showData()
+        {
+            var getDriver = (from Order o in DB.conn
+                             join Driver d in DB.conn
+                             on o.driverID equals d.ID
+                             group o by new { o.driverID, d.name } into g
+                             let count = g.Count()
+                             orderby count descending
+                             select new
+                             {
+                                 g.Key.driverID,
+                                 Name = g.Key.name,
+                                 Trips = count
+                             }).Take(10).ToList();
+
+            dgvDriver.DataSource = getDriver;
         }
     }
 }
