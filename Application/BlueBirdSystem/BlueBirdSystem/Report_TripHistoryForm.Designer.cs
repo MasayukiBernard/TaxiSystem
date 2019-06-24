78f08502069bcdbe04150d921a25c9f97f1bf91c
Amadeus Darren Leander <41075353+adarrenleander@users.noreply.github.com>
2019-06-24 23:28:21 +0700

    Updated all files
    Final version

--- Application/BlueBirdSystem/BlueBirdSystem/Report_TripHistoryForm.Designer.cs
+++ Application/BlueBirdSystem/BlueBirdSystem/Report_TripHistoryForm.Designer.cs
@@ -28,12 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Month = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Year = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Month
+            // 
+            this.Month.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Month.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Month.FormattingEnabled = true;
+            this.Month.Items.AddRange(new object[] {
+            "-- Choose Month --",
+            "January",
+            "Febuary",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"});
+            this.Month.Location = new System.Drawing.Point(316, 89);
+            this.Month.Name = "Month";
+            this.Month.Size = new System.Drawing.Size(219, 33);
+            this.Month.TabIndex = 5;
+            this.Month.SelectedIndexChanged += new System.EventHandler(this.Month_SelectedIndexChanged_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(231, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Month";
+            // 
+            // Year
+            // 
+            this.Year.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Year.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Year.FormattingEnabled = true;
+            this.Year.Items.AddRange(new object[] {
+            "-- Choose Year --",
+            "2018",
+            "2019"});
+            this.Year.Location = new System.Drawing.Point(316, 35);
+            this.Year.Name = "Year";
+            this.Year.Size = new System.Drawing.Size(219, 33);
+            this.Year.TabIndex = 6;
+            this.Year.SelectedIndexChanged += new System.EventHandler(this.Year_SelectedIndexChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(231, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Year";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 141);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 278);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Report_TripHistoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Report_TripHistoryForm";
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Month);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Year);
+            this.Controls.Add(this.label1);
+            this.Name = "Report_TripHistoryForm";
+            this.Text = "Blue Bird - Report: Trip History";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox Month;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox Year;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Application/BlueBirdSystem/BlueBirdSystem/Report_TripHistoryForm.cs
+++ Application/BlueBirdSystem/BlueBirdSystem/Report_TripHistoryForm.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Db4objects.Db4o.Linq;
+using BlueBirdSystem.data;
+using BlueBirdSystem.model;
 
 namespace BlueBirdSystem
 {
@@ -15,6 +18,128 @@ namespace BlueBirdSystem
         public Report_TripHistoryForm()
         {
             InitializeComponent();
+            resetInitial();
+            showData();
+        }
+        public void resetInitial()
+        {
+
+            Year.SelectedIndex = 0;
+            Month.SelectedIndex = 0;
+            Year.Enabled = true;
+            Month.Enabled = false;
+        }
+
+
+        public void showData()
+        {
+
+            var data = (from Order o in DB.conn
+                        join Driver d in DB.conn on o.driverID equals d.ID
+                        join Customer c in DB.conn on o.customerID equals c.ID
+                        join Payment p in DB.conn on o.paymentID equals p.ID
+                        where o.completed == true
+                        select new
+                        {
+                            OrderID = o.ID,
+                            CustomerID = c.ID,
+                            DriverID = d.ID,
+                            PaymentID = p.ID,
+                            OrderDate = o.orderDate,
+                            PickupLocation = o.pickupLocation,
+                            DestinationLocation = o.destinationLocation,
+                            TravelDuration = o.travelDuration
+
+
+                        }).ToList();
+
+            dataGridView1.DataSource = data;
+        }
+        public void showDataYear()
+        {
+            if (Year.SelectedIndex != 0)
+            {
+                var year = Int32.Parse(Year.Text);
+                var data = (from Order o in DB.conn
+                            join Driver d in DB.conn on o.driverID equals d.ID
+                            join Customer c in DB.conn on o.customerID equals c.ID
+                            join Payment p in DB.conn on o.paymentID equals p.ID
+                            where o.completed == true && o.orderDate.Year == year
+                            select new
+                            {
+                                OrderID = o.ID,
+                                CustomerID = c.ID,
+                                DriverID = d.ID,
+                                PaymentID = p.ID,
+                                OrderDate = o.orderDate,
+                                PickupLocation = o.pickupLocation,
+                                DestinationLocation = o.destinationLocation,
+                                TravelDuration = o.travelDuration
+
+
+                            }).ToList();
+
+                dataGridView1.DataSource = data;
+            }
+
+        }
+
+      
+        
+
+        
+
+        private void Year_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            {
+                Month.SelectedIndex = 0;
+                if (Year.SelectedIndex == 0)
+                {
+                    showData();
+                    Month.Enabled = false;
+                }
+                else if (Year.SelectedIndex != 0)
+                {
+                    showDataYear();
+                    Month.Enabled = true;
+                }
+            }
+        }
+
+        private void Month_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (Month.SelectedIndex == 0)
+            {
+                showDataYear();
+            }
+            else
+            {
+                var year = Int32.Parse(Year.Text);
+                var month = Month.SelectedIndex;
+                var data = (from Order o in DB.conn
+                            join Driver d in DB.conn on o.driverID equals d.ID
+                            join Customer c in DB.conn on o.customerID equals c.ID
+                            join Payment p in DB.conn on o.paymentID equals p.ID
+                            where o.completed == true && o.orderDate.Year == year && o.orderDate.Month == month
+                            select new
+                            {
+                                OrderID = o.ID,
+                                CustomerID = c.ID,
+                                DriverID = d.ID,
+                                PaymentID = p.ID,
+                                OrderDate = o.orderDate,
+                                PickupLocation = o.pickupLocation,
+                                DestinationLocation = o.destinationLocation,
+                                TravelDuration = o.travelDuration
+
+
+                            }).ToList();
+
+                dataGridView1.DataSource = data;
+            }
         }
     }
+
 }
+
+
